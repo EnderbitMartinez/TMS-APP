614544d8986021b8dfaa3f1959d02e54fbc3248e
EnderbitMartinez <martinezenderbit@gmail.com>
2026-09-09 11:42:24 -0400

    Add Supabase connection configuration

---
+++ config.js
@@ -0,0 +1,5 @@
+// Configuración de conexión a Supabase
+const SUPABASE_URL = 'https://dtqwwpskztchtsetvhfw.supabase.co/rest/v1/';
+const SUPABASE_ANON_KEY = 'sb_publishable_ibFRbll0lV6bcqelIZfY0g_Z3RLYyuz';
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
