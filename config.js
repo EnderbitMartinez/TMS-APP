8a874bea6e3e1f5448fae41eaf845f8e2dac8a85
EnderbitMartinez <martinezenderbit@gmail.com>
2026-09-09 12:59:31 -0400

    Update Supabase URL and ANON key format

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Configuración de conexión a Supabase
-const SUPABASE_URL = 'https://dtqwwpskztchtsetvhfw.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_ibFRbl10lV6bcqeLIZfY0g_Z3RLYyuz';
+const SUPABASE_URL = 'https://dtqwwpskztchtsetvhfw.supabase.co/rest/v1/';
+const SUPABASE_ANON_KEY = 'sb_publishable_ibFRbll0lV6bcqelIZfY0g_Z3RLYyuz';
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
