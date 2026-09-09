3aafbf2377c50d290c705de536d11af50929b957
EnderbitMartinez <martinezenderbit@gmail.com>
2026-09-09 13:04:03 -0400

    Change Supabase anonymous key in config.js
    Updated Supabase anonymous key for connection.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Configuración de conexión a Supabase
 const SUPABASE_URL = 'https://dtqwwpskztchtsetvhfw.supabase.co/rest/v1/';
-const SUPABASE_ANON_KEY = 'sb_publishable_ibFRbll0lV6bcqelIZfY0g_Z3RLYyuz';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR0cXd3cHNrenRjaHRzZXR2aGZ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg5NjA0MjksImV4cCI6MjEwNDUzNjQyOX0.sXggVarAzb1swMW707UDNTnfojETJCx3XYasKCxbwCI';
 
 const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
